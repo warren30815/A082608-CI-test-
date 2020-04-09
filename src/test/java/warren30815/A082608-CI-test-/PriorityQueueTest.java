9b7450ce995c382ff3b3faea1fefee28f5319a31
Jordan Hsu <qaz7821819@gmail.com>
2020-04-09 23:48:07 +0800

    Pass all testcases

--- src/test/java/warren30815/A082608-CI-test-/PriorityQueueTest.java
+++ src/test/java/warren30815/A082608-CI-test-/PriorityQueueTest.java
@@ -42,7 +42,7 @@ public class PriorityQueueTest {
                 { 3,1,4,2 },
         };
         int[][] case5 = new int[][]{
-                { 1,2,3,5 },
+                { 1,2,3,4 },
                 { 3,4,2,1 },
         };
         paramater.add(case1);
